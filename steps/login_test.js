9b138cb84048a1277a917a7c162e8c815aa0e36d
Mateus GitHub <mateusasfreitas@gmail.com>
2022-06-02 20:47:04 -0300

    Removing Before and After

--- steps/login_test.js
+++ steps/login_test.js
@@ -2,26 +2,6 @@ Feature('login');
 
 const { login_page } = inject() //global import, because login will be used in every scenario
 
-BeforeSuite (() => {
-    console.log("Before Suite")
-
-});
-
-Before (() => {
-    console.log("Before")
-
-});
-
-AfterSuite (() => {
-    console.log("After Suite")
-
-});
-
-After (() => {
-    console.log("After")
-
-});
-
 Scenario('Login with success', ({ I, home_page }) => {
     login_page.doLogin('teste@teste.com', '123456')
     home_page.checkLoginSuccess()
