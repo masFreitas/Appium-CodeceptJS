153249d87d46479313f87dc2170b9f69a1fb763d
Mateus GitHub <mateusasfreitas@gmail.com>
2022-06-02 21:03:32 -0300

    Added Tags

--- steps/login_test.js
+++ steps/login_test.js
@@ -1,13 +1,13 @@
-Feature('login');
+Feature('login').tag('@login');
 
 const { login_page } = inject() //global import, because login will be used in every scenario
 
 Scenario('Login with success', ({ I, home_page }) => {
     login_page.doLogin('teste@teste.com', '123456')
     home_page.checkLoginSuccess()
-});
+}).tag('@loginsucesso');
 
 Scenario('Login with error', ({ I }) => {
     login_page.doLogin('xteste@teste.com', '123456')
     login_page.checkLoginError()
-});
+}).tag('@login_erro');
